51e6ab4ce4d54842ecdd7f6ebb7bc5b741f8bfd2
Samuel Crisóstomo <samueld162@gmail.com>
2023-01-04 08:03:34 -0300

    Criação do campo de texto e passagem de parametro com props. Aula 1.6 e Aula1.7

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Banner from './componentes/Banner/Banner';
+import CampoTexto from './componentes/CampoTexto';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <CampoTexto placeholder="Digite seu nome" label="Nome" />
+      <CampoTexto placeholder="Digite seu Cargo" label="Cargo" />
+      <CampoTexto placeholder="Informe o endereço da imagem" label="Imagem" />
     </div>
   );
 }
