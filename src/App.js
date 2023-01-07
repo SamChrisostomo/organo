ffd8fab2223f792f1cc1678e1baa2450816bd52b
Samuel Crisóstomo <samueld162@gmail.com>
2023-01-07 12:02:08 -0300

    Modificação do arquivo App.js adicionando o componente formulário

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import Banner from './componentes/Banner/Banner';
-import CampoTexto from './componentes/CampoTexto';
+import Banner from './componentes/Banner';
+import Formulario from './componentes/Formulario';
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <CampoTexto placeholder="Digite seu nome" label="Nome" />
-      <CampoTexto placeholder="Digite seu Cargo" label="Cargo" />
-      <CampoTexto placeholder="Informe o endereço da imagem" label="Imagem" />
+      <Formulario />
     </div>
   );
 }
