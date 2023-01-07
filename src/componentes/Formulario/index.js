909dd1577781cb0250502cd6aa33d1ec43f90a51
Samuel Crisóstomo <samueld162@gmail.com>
2023-01-07 11:48:40 -0300

    Modificação do arquivos index.js adicionando o componentes de ListaSuspensa

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,7 +1,17 @@
 import CampoTexto from "../CampoTexto"
+import ListaSuspensa from "../ListaSuspensa"
 import "./Formulario.css"
 
 const Formulario = () => {
+    const time = [
+        'Programação',
+        'Front-end',
+        'DevOps',
+        'UX e Design',
+        'Mobile',
+        'Gestão e Inovação'
+    ]
+
     return (
         <section className="formulario">
             <form>
@@ -9,6 +19,7 @@ const Formulario = () => {
                 <CampoTexto placeholder="Digite seu nome" label="Nome" />
                 <CampoTexto placeholder="Digite seu Cargo" label="Cargo" />
                 <CampoTexto placeholder="Informe o endereço da imagem" label="Imagem" />
+                <ListaSuspensa itens={time} label="Times" />
             </form>
         </section>
     )
