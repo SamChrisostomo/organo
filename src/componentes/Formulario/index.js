34281ef3cedb6ea88b3a9ee02e9cb3b9f42be624
Samuel Crisóstomo <samueld162@gmail.com>
2023-01-07 12:01:12 -0300

    Modificação do arquivos index.js adicionando o componente Botao

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,3 +1,4 @@
+import Botao from "../Botao"
 import CampoTexto from "../CampoTexto"
 import ListaSuspensa from "../ListaSuspensa"
 import "./Formulario.css"
@@ -20,6 +21,9 @@ const Formulario = () => {
                 <CampoTexto placeholder="Digite seu Cargo" label="Cargo" />
                 <CampoTexto placeholder="Informe o endereço da imagem" label="Imagem" />
                 <ListaSuspensa itens={time} label="Times" />
+                <Botao>
+                    Criar Card
+                </Botao>
             </form>
         </section>
     )
